e46530351dedcf205dc372eed6ab0a375bc7f8cd
andyandriadoria <andyandriadoria@gmail.com>
2026-09-16 11:20:30 +0700

    release: align API version with v0.6.3

--- backend/src/worker.js
+++ backend/src/worker.js
@@ -2,7 +2,7 @@ import app from './index.js';
 import { corsHeaders, json, withCors } from './http.js';
 import { ensureProductionLaunchBaseline } from './launch-baseline.js';
 
-const VERSION = '0.6.2';
+const VERSION = '0.6.3';
 let baselineSnapshot = null;
 let baselineInflight = null;
 
